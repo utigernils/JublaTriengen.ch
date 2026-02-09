bc8f8b2f332216b7bab8fb1abcf4d91d098f551c
utigernils <utigernils@gmail.com>
2026-02-09 21:35:40 +0100

    refactor; move main page to landing

--- JublaTriengen/src/main.tsx
+++ JublaTriengen/src/main.tsx
@@ -1,17 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './pages/placeholder/App.tsx'
+import Landing from './pages/Landing/Landing.tsx'
 import NotFound from './pages/NotFound/NotFound.tsx'
 
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
-  {path:"/", element: <App />},
+  {path:"/", element: <Landing />},
   {path: "*", element: <NotFound />}
 ]);
 
 
 createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
 )
