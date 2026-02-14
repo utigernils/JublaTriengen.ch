087cb4dd54674f2d715fc73a11c1335e5af8f488
utigernils <utigernils@gmail.com>
2026-02-14 11:54:20 +0100

    refactor; make sure components are used

---
+++ src/components/ui/SectionText.tsx
@@ -0,0 +1,12 @@
+interface SectionTextProps {
+  children: string;
+  className?: string;
+}
+
+export default function SectionText({ children, className = '' }: SectionTextProps) {
+  return (
+    <p className={`font-inter font-light text-xl text-center text-gray-600 max-w-3xl mx-auto ${className}`}>
+      {children}
+    </p>
+  );
+}
